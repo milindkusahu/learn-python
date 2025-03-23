b13fe0b078b40cedcb5161000404e736963a8e00
milindkusahu <milindsahu2000@gmail.com>
2025-03-23 20:14:04 +0530

    01.) Add hello_world module and chai function with initial implementation

---
+++ 01_basics/chai.py
@@ -0,0 +1,4 @@
+# This is the main file to run the chai function from hello_world module
+from hello_world import chai
+
+chai("Ginger Tea")
---
+++ 01_basics/hello_world.py
@@ -0,0 +1,10 @@
+# This is a simple hello world program
+print("Hello world")
+
+# This is a simple function
+def chai(n):
+  print(n)
+
+chai(4)
+chai("Lemon Tea")
+
